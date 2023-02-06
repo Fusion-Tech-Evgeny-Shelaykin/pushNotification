d13d4da54462ef475eec8b72853cb1b112810ccc
Evgeniy Shelaykin <evgeny.shelaykin@fusion-tech.pro>
2023-02-06 23:55:47 +0300

    addSvg

--- metro.config.js
+++ metro.config.js
@@ -19,6 +19,7 @@ module.exports = (async () => {
           inlineRequires: true,
         },
       }),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
