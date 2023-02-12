436fc112cd578982e4b104420a7dd4524651ac9d
Evgeniy Shelaykin <evgeny.shelaykin@fusion-tech.pro>
2023-02-12 20:50:05 +0300

    disableMiddleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit';
+
 import pokeReducer from './poke/reducer';
+
+const middlewares = [];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  middlewares.push(createDebugger());
+}
 
 export const store = configureStore({
   reducer: {
     poke: pokeReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat([...middlewares]),
 });
 
 export type RootStore = ReturnType<typeof store.getState>;
